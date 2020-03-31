6e09707b985fa57abf1d3ea389e426ffdbaa4bc2
hyeonbeom92 <hyeonbeom92@churchofjesuschrist.org>
2020-03-30 19:12:22 -0600

    hamburger button

--- Hamburger Button/script.js
+++ Hamburger Button/script.js
@@ -1,35 +1,25 @@
 const hamburgerButton = document.getElementById("hamburgerButton");
-console.log(hamburgerButton);
 const nav = document.getElementById("nav");
 const sign_up_button = document.getElementById("sign_up_button");
 const form_container = document.getElementById("form-container");
 const cancel_button = document.getElementById("cancel_button");
 const submit_button = document.getElementById("submit_button");
-// let opened = false;
-// nav.style.display = "none";
-// no click but call.
-hamburgerButton.addEventListener("click", showNav);
-sign_up_button.addEventListener("click", showForm);
+
+hamburgerButton.addEventListener("click", () => {
+  document.body.classList.toggle("showNav");
+});
+sign_up_button.addEventListener("click", () => {
+  form_container.classList.add("displayModal", true);
+});
 cancel_button.addEventListener("click", () => {
   form_container.classList.remove("displayModal");
 });
-submit_button.addEventListener("click", () => {
+submit_button.addEventListener("click", e => {
+  e.preventDefault();
   alert("submit");
 });
-
-function showNav() {
-  //   console.log("working?");
-
-  // console.log("false");
-  // if I delete this, it does not work.
-  //   nav.classList.toggle("nav");
-
-  //   document.body.style.transform = "translateX(15rem)";
-  document.body.classList.toggle("showNav");
-}
-
-function showForm() {
-  // document.body.classList.toggle("overlay");
-  // form_container.classList.toggle("form-container");
-  form_container.classList.add("displayModal", true);
-}
+document.body.addEventListener("click", e => {
+  e.target === form_container
+    ? form_container.classList.remove("displayModal")
+    : "";
+});
