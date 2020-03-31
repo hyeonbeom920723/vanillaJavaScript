0a7cf45a38304f720fe495d8b0ecefcd894f3139
hyeonbeom92 <hyeonbeom92@churchofjesuschrist.org>
2020-03-30 18:33:42 -0600

    working on the hamburger button

--- Hamburger Button/script.js
+++ Hamburger Button/script.js
@@ -1,12 +1,21 @@
-const hamburgerButton = document.getElementById("container");
+const hamburgerButton = document.getElementById("hamburgerButton");
 console.log(hamburgerButton);
 const nav = document.getElementById("nav");
-const sign_up_button = document.getElementById("sign_up");
+const sign_up_button = document.getElementById("sign_up_button");
+const form_container = document.getElementById("form-container");
+const cancel_button = document.getElementById("cancel_button");
+const submit_button = document.getElementById("submit_button");
 // let opened = false;
 // nav.style.display = "none";
 // no click but call.
 hamburgerButton.addEventListener("click", showNav);
 sign_up_button.addEventListener("click", showForm);
+cancel_button.addEventListener("click", () => {
+  form_container.classList.remove("displayModal");
+});
+submit_button.addEventListener("click", () => {
+  alert("submit");
+});
 
 function showNav() {
   //   console.log("working?");
@@ -17,4 +26,10 @@ function showNav() {
 
   //   document.body.style.transform = "translateX(15rem)";
   document.body.classList.toggle("showNav");
+}
+
+function showForm() {
+  // document.body.classList.toggle("overlay");
+  // form_container.classList.toggle("form-container");
+  form_container.classList.add("displayModal", true);
 }
