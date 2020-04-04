ebbdb70fec6c8878ccfd017104fd8c281ff6d1b1
hyeonbeom920723 <hyeonbeom920723@gmail.com>
2020-04-04 10:50:06 -0600

    done with display and create transaction

--- Money manager/script.js
+++ Money manager/script.js
@@ -1,51 +1,77 @@
-const display_each_history = document.getElementById("display_each_history");
-const transaction_button = document.getElementById("transaction_button");
-const transaction_item = document.getElementById("transaction_item");
-const transaction_amount = document.getElementById("transaction_amount");
-const current_balance = document.getElementById("current_balance");
-console.log("dsfsad" + transaction_item);
-console.log(transaction_amount);
+const form = document.getElementById("form");
+const ul = document.getElementById("display_each_history");
+const transaction_label_el = document.getElementById("transaction_label");
+const transaction_amount_el = document.getElementById("transaction_amount");
 
-function calCurrent_balance() {}
-
-let templateTransactions = [
+let transactions = [
   {
     label: "Cash Income",
-    value: 500,
+    amount: 500,
   },
   {
     label: "Grocery",
-    value: -150,
+    amount: -150,
   },
   {
     label: "Entertainment",
-    value: -50,
+    amount: -50,
   },
   {
     label: "Tithing",
-    value: -100,
+    amount: -100,
   },
 ];
 
-function getType(value) {
-  console.log(value);
-  if (value >= 0) {
-    return "display_each_income";
-  } else {
-    return "display_each_expense";
-  }
-  //   what is differnce between above and this
-  // value >= 0 ? "display_each_income" : "display_each_expense";
+function displayTransaction(new_t) {
+  // make list
+  const list = document.createElement("li");
+  // put class in the list
+  list.classList.add("each_history_container", true);
+  // put span in the list
+
+  list.innerHTML = `<i class="fas fa-times fa-x deleteHistory"></i>${
+    new_t.label
+  }<span class=${
+    new_t.amount >= 0 ? "display_each_income" : "display_each_expense"
+  }>${new_t.amount}</span>`;
+  // put list in the ul (append)
+  ul.appendChild(list);
 }
 
-const markup = templateTransactions
-  .map(
-    (each) => `
-    <li class="each_history_container">
-        <i class="fas fa-times fa-x deleteHistory"></i>${each.label}
-        <span class="${getType(each.value)}">${each.value}</span>
-    </li>`
-  )
-  .join("");
-console.log(markup);
-display_each_history.innerHTML = markup;
+function createTransaction(event) {
+  // when I use eventListener form, we use this.
+  event.preventDefault();
+  // if input is empty alret
+  // transaction_label_el.value === "" || transaction_amount_el.value === ""
+  //   ? alert("required inputs")
+  //   : "";
+  // if input exist, make object that has label value and amout value
+  console.log(transaction_label_el.value);
+  console.log(transaction_amount_el.value);
+  if (transaction_label_el.value === "" || transaction_amount_el.value === "") {
+    alert("required inputs");
+  } else {
+    const new_t = {
+      label: transaction_label_el.value,
+      amount: transaction_amount_el.value,
+    };
+    // }
+    // push the new object into the transactions
+    transactions.push(new_t);
+    // reset the value for transaction label and amount
+    transaction_label_el.value = "";
+    transaction_amount_el.value = "";
+    // display the new object by calling displayTranscation
+    displayTransaction(new_t);
+  }
+}
+
+function init() {
+  transactions.forEach((each_t) => {
+    displayTransaction(each_t);
+  });
+}
+
+init();
+
+form.addEventListener("submit", createTransaction);
