419dfcb7942a16f94ca41431a56dba43221390e4
hyeonbeom920723 <hyeonbeom920723@gmail.com>
2020-04-05 10:02:22 -0600

    finish the money manager

--- Money manager/script.js
+++ Money manager/script.js
@@ -2,9 +2,29 @@ const form = document.getElementById("form");
 const ul = document.getElementById("display_each_history");
 const transaction_label_el = document.getElementById("transaction_label");
 const transaction_amount_el = document.getElementById("transaction_amount");
+const display_income_el = document.getElementById("display_income");
+const display_expense_el = document.getElementById("display_expense");
+const display_current_balance_el = document.getElementById("current_balance");
 
 let transactions = [];
+function calSummary() {
+  // make a new array only for amount from transactions
+  let amountArray = [];
+  amountArray = transactions.map((t) => +t.amount);
+  // from the new array, sum positive and sum negative numbers filter -> new array
+  const positiveNumbers = amountArray.filter((each_amount) => each_amount >= 0);
+  const negativeNumbers = amountArray.filter((each_amount) => each_amount < 0);
 
+  // sum each numbers
+  const totalPositive = positiveNumbers.reduce((a, b) => a + b, 0);
+  const totalNegative = negativeNumbers.reduce((a, b) => a + b, 0);
+
+  // display each thing on the HTML
+  display_income_el.innerHTML = "$" + totalPositive;
+  display_expense_el.innerHTML = "$" + Math.abs(totalNegative);
+  const current_balance = totalPositive + totalNegative;
+  display_current_balance_el.innerHTML = "$" + current_balance;
+}
 function displayTransaction(new_t) {
   // make list
   const list = document.createElement("li");
@@ -24,13 +44,6 @@ function displayTransaction(new_t) {
 function createTransaction(event) {
   // when I use eventListener form, we use this.
   event.preventDefault();
-  // if input is empty alret
-  // transaction_label_el.value === "" || transaction_amount_el.value === ""
-  //   ? alert("required inputs")
-  //   : "";
-  // if input exist, make object that has label value and amout value
-  console.log(transaction_label_el.value);
-  console.log(transaction_amount_el.value);
   if (transaction_label_el.value === "" || transaction_amount_el.value === "") {
     alert("required inputs");
   } else {
@@ -38,13 +51,13 @@ function createTransaction(event) {
       label: transaction_label_el.value,
       amount: transaction_amount_el.value,
     };
-    // }
     // push the new object into the transactions
     transactions.push(new_t);
 
     // display the new object by calling displayTranscation
     displayTransaction(new_t);
 
+    calSummary();
     // reset the value for transaction label and amount
     transaction_label_el.value = "";
     transaction_amount_el.value = "";
