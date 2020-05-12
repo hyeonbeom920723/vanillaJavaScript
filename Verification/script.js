5219f295bf2eb37062fd635ffe1c4058439ab061
hyeonbeom920723 <hyeonbeom920723@gmail.com>
2020-05-12 09:53:44 +0900

    working on the verification

--- Verification/script.js
+++ Verification/script.js
@@ -1,41 +1,33 @@
 // move cursor
 
 // if number is correct alert
-const correct_codeList = [1, 2, 3, 4];
+const correct_code = [1, 2, 3, 4];
 const user_input = [];
 const code = document.getElementsByClassName("code");
 const code_container_el = document.getElementById("code_containe");
-// console.log("code", code[0]);
-// console.log("code", code[1]);
-// console.log("code", code[2]);
-// console.log("code", code[3]);
+const first_input_el = document.getElementById("first_input");
 
+first_input_el.focus();
+function check() {
+  if (
+    correct_code.length === user_input.length &&
+    correct_code.every((value, index) => {
+      return value === correct_code[index];
+    })
+  ) {
+    alert("You have entered correct code.");
+  } else {
+    alert("You have entered wrong code.");
+    location.reload();
+  }
+}
 // display four blank underscore
 //
 // focus the first one
 // get the input from the user
 
 // if the input is same
-function displayCode() {
-  //   code_container_el.innerHTML = codeList.map((each_code) => {
-  //     if (each_code === )
-  //   })
-  //   = randomWord
-  //     .split("") // "bac".split(""); // ["b", "a", "c"]
-  //     .map((letter) => {
-  //       return `<span class="letter">${
-  //         correct_letters.includes(letter) ? letter : ""
-  //       }</span>`;
-  //     })
-  //     .join("");
-  //   console.log("old one " + randomWord);
-  //   // console.log(wordElement.innerText);
-  //   const wordElement_string = wordElement.innerText.replace(/\n/g, "");
-  //   // console.log(randomWord === wordElement_string);
-  //   randomWord === wordElement_string
-  //     ? (notification_container.style.display = "flex")
-  //     : "";
-}
+function displayCode() {}
 
 window.addEventListener("keydown", (e) => {
   const number = parseInt(e.key.toLocaleLowerCase());
@@ -54,4 +46,5 @@ window.addEventListener("keydown", (e) => {
 function init() {
   displayCode();
 }
+
 init();
