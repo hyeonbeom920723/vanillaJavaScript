f6786af2bbcc6039e297294d54ff9edf3ac92c98
hyeonbeom920723 <hyeonbeom920723@gmail.com>
2020-05-19 06:58:41 +0900

    save

--- Speech/script.js
+++ Speech/script.js
@@ -1,4 +1,6 @@
 const main_el = document.getElementById("main");
+const callModal_el = document.getElementById("callModal");
+const modal_el = document.getElementById("modal");
 
 const data = [
   {
@@ -51,6 +53,12 @@ const data = [
   },
 ];
 
+var synth = window.speechSynthesis;
+var voices = [];
+function populateVoiceList() {
+  voices = synth.getVoices();
+}
+
 // console.log(data[0].image);
 
 function createCards(image, text) {
@@ -58,14 +66,19 @@ function createCards(image, text) {
   cardContainer.classList.add("card_container");
   cardContainer.innerHTML = `
     <div class="image_container">
-        <img src=${image} alt="card information" />
+        <img src=${image} alt="${text}" />
     </div>
     <p class="card_description">
         ${text}
     </p>`;
   //   console.log(main_el); // null !!!!
-  console.log(cardContainer); // working good
-  //   main_el.appendChild(cardContainer);
+  //   console.log(cardContainer); // working good
+  main_el.appendChild(cardContainer);
+
+  cardContainer.addEventListener("click", () => {
+    // handle when card id clicked
+    // synth.speak("speak");
+  });
 }
 // data.forEach(createCards);
 data.forEach((e, index) => {
